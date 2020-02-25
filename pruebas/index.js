f153ee1e7f8475577cf54432685a9178846caa02
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-24 21:20:38 -0300

    agregando el modulo de pruebas

---
+++ pruebas/index.js
@@ -0,0 +1,7 @@
+//const lib = require('./../introducion-npm/index');
+const lib = require('introducion-node');
+
+lib.hola();
+lib.holaNPM();
+lib.holaPersonalizado('cristian');
+lib.holaPersonalizado('chloe moretz');
